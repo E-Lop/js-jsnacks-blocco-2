6bf59192b43b857c4f1c72e39d583a3c08493ea4
E-Lop <edmondolopez@gmail.com>
2022-06-10 14:20:01 +0200

    creati due array di partenza

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,3 +1,9 @@
 /* Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi casuali all’array che ha meno elementi,
 fino a quando ne avrà tanti quanti l’altro. */
+
+// array con pochi elementi
+const shortArray = [2, 6, 7, 9];
+
+// array con molti elementi
+const longArray = [1, 3, 5, 11, 13, 15, 17, 19, 42];
