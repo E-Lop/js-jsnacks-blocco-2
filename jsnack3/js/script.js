2b0a809d1b0e415e45d542542f630f6596e31682
E-Lop <edmondolopez@gmail.com>
2022-06-10 14:13:25 +0200

    esercizio finito

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,31 @@
 /* Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while. */
+
+// variabile flag per la somma dei numeri dati dall'utente
+let somma = 0;
+
+/***** INIZIO CALCOLO CON FOR ******/
+// generazione dei 5 prompt
+/* for (let i = 0; i < 5; i++) {
+  const userNumber = parseInt(prompt('Scrivi un numero'));
+//   somma del numero attuale alla somma dei numeri precedenti
+  somma += userNumber;
+} */
+
+// stampa della somma totale
+/* console.log('Somma di tutti i numeri', somma); */
+/***** FINE CALCOLO CON FOR ******/
+
+/***** INIZIO CALCOLO CON WHILE *****/
+// generazione dei 5 prompt
+let i = 0;
+while (i < 5) {
+  const userNumber = parseInt(prompt('Scrivi un numero'));
+  //   somma del numero attuale alla somma dei numeri precedenti
+  somma += userNumber;
+  i++;
+}
+
+// stampa della somma totale
+console.log('Somma di tutti i numeri', somma);
