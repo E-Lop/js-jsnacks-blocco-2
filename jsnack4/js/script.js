e96f3063b1e1cc9bccb9aac8fbea6360658c828f
E-Lop <edmondolopez@gmail.com>
2022-06-10 14:28:43 +0200

    esercizio finito

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -6,4 +6,14 @@ fino a quando ne avrà tanti quanti l’altro. */
 const shortArray = [2, 6, 7, 9];
 
 // array con molti elementi
-const longArray = [1, 3, 5, 11, 13, 15, 17, 19, 42];
+const longArray = [1, 3, 5, 11, 13, 15, 17, 19, 42, 77];
+
+// finchè longArray avrà più elementi di shortArray
+while (longArray.length > shortArray.length) {
+  // aggiungi un numero casuale da 1 a 100 a shortArray
+  shortArray.push(Math.floor(Math.random() * 100) + 1);
+}
+
+// stampa in console di verifica
+console.log('array corto', shortArray);
+console.log('array lungo', longArray);
